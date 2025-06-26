c47338045cfee3f477447d38a543325cc7e0d4f1
Malyhaxor <malyhaxor@gmail.com>
2025-06-26 12:52:11 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,6 @@
+const points = [
+  { label: "START", lat: 52.230, lng: 21.010 },
+  { label: "PKC", lat: 52.233, lng: 21.013 },
+  { label: "META", lat: 52.237, lng: 21.020 },
+  { label: "SOS", lat: 52.235, lng: 21.017 }
+];
